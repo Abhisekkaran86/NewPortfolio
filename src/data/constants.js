8f39419b47158685b12400d4b9d14044a0ba3b34
Abhisek Karan <143966435+Abhisekkaran86@users.noreply.github.com>
2025-08-26 20:17:40 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -207,25 +207,25 @@ export const projects = [
   github: "https://github.com/Abhisekkaran86/JewelleryShopping",
   webapp: "",
   },
-  // {
-  //   id: 13,
-  //   title: "Electra-Mate",
-  //   date: "Jun 2024 - Aug 2024",
-  //   description:
-  //     "Electra-Mate is an electronic service request platform built with Spring Boot microservices, React JS, and MySQL. It enables users to submit repair requests, and automates email notifications via Google OAuth 2.0. Supports both user and service provider roles.",
-  //   image: electraMateLogo, // Replace with actual image URL
-  //   tags: [
-  //     "Java",
-  //     "Spring Boot",
-  //     "Microservices",
-  //     "React JS",
-  //     "MySQL",
-  //     "Google OAuth 2.0",
-  //   ],
-  //   category: "web app",
-  //   github: "https://github.com/CoderAvijit/electra-mate", // Replace with actual link
-  //   webapp: "https://riju-electronics.vercel.app/", // Replace with actual URL
-  // },
+  {
+    id: 13,
+    title: "Electra-Mate",
+    date: "Jun 2024 - Aug 2024",
+    description:
+      "Electra-Mate is an electronic service request platform built with Spring Boot microservices, React JS, and MySQL. It enables users to submit repair requests, and automates email notifications via Google OAuth 2.0. Supports both user and service provider roles.",
+    image: electraMateLogo, // Replace with actual image URL
+    tags: [
+      "Java",
+      "Spring Boot",
+      "Microservices",
+      "React JS",
+      "MySQL",
+      "Google OAuth 2.0",
+    ],
+    category: "web app",
+    github: "https://github.com/CoderAvijit/electra-mate", // Replace with actual link
+    webapp: "https://riju-electronics.vercel.app/", // Replace with actual URL
+  },
   //   {
   //   id: 14,
   //   title: "Echo - Voice-Activated Personal Assistant Bot",
